ae8e78ed3902db573c7408467f56c05bbdf8603a
Alloyshoes <aloysiusgohh362@gmail.com>
2026-08-21 00:54:36 +0800

    Rename chatbot from Duke to Eve; implement greet-and-exit
    - Rename Duke.java to Eve.java; update ASCII banner to spell EVE
    - Print greeting/farewell messages wrapped in divider lines, per the
      'greet the user and exit' requirement
    - Update README.md and docs/README.md references from Duke to Eve

---
+++ src/main/java/Eve.java
@@ -0,0 +1,19 @@
+public class Eve {
+    public static void main(String[] args) {
+        String banner = " _____  __   __  _____ \n"
+                + "|  ___| \\ \\ / / |  ___|\n"
+                + "| |__    \\ V /  | |__  \n"
+                + "|  __|    \\ /   |  __| \n"
+                + "|_____|    V    |_____|";
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Eve.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
