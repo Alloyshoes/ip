760d5542147d3e0a6153895d9da64e0226c36319
Alloyshoes <aloysiusgohh362@gmail.com>
2026-08-21 01:31:42 +0800

    Implement command echo with exit on 'bye'
    - Read user input in a loop via Scanner
    - Echo each command back wrapped in divider lines
    - On 'bye', print farewell message and exit the loop

--- src/main/java/Eve.java
+++ src/main/java/Eve.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Eve {
     public static void main(String[] args) {
         String banner = " _____  __   __  _____ \n"
@@ -13,7 +15,20 @@ public class Eve {
         System.out.println("What can I do for you?");
         System.out.println(line);
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            else {
+                System.out.println(line);
+                System.out.println(input);
+                System.out.println(line);
+            }
+        }
+        scanner.close();
     }
 }
